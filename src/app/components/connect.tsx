c5577f66ecc7e66762c1cc66634e881515b192e0
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-09-16 13:40:39 +0530

    responsive design fixes

--- src/app/components/connect.tsx
+++ src/app/components/connect.tsx
@@ -10,7 +10,7 @@ const connect = () => {
     <div className='flex justify-center mb-[100px] sm:mb-0'>
       <div className='h-[60vh] w-[90%] flex flex-col justify-center items-center'>
         <div className='text-4xl mb-8'>Lets Connect</div>
-        <div className='text-sm text-slate-400'>As a passionate and dedicated professional, I'm always eager to take on new challenges and contribute to exciting projects. Whether you're looking for a skilled developer, a creative thinker, or a collaborative team member, I'm ready to bring my expertise to your next venture. My inbox is always open, and I'm enthusiastic about connecting with like-minded individuals and companies. Let's explore how we can work together to create something extraordinary!</div>
+        <div className='text-sm text-slate-400 md:text-left text-center'>As a passionate and dedicated professional, I'm always eager to take on new challenges and contribute to exciting projects. Whether you're looking for a skilled developer, a creative thinker, or a collaborative team member, I'm ready to bring my expertise to your next venture. My inbox is always open, and I'm enthusiastic about connecting with like-minded individuals and companies. Let's explore how we can work together to create something extraordinary!</div>
         <div>
             <div className='flex justify-center items-center gap-[10px] mt-8 cursor-pointer'>
                 <FontAwesomeIcon icon={faEnvelope} style={{ color: '#74C0FC' }} size="2x" />

6f7828ee96b3ee87097794489c43a2e330982d85
KrishnaGitHub18 <krishnadeheriya1803@gmail.com>
2024-09-17 22:18:58 +0530

    silly mistake fixed

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -4,7 +4,7 @@ const Page = () => {
     return (
         <nav className='fixed w-[97vw] mx-4 bg-zinc-900 bg-opacity-100 z-50 top-0 mr-[1vw]'>
             <div className='p-4 flex md:justify-between mb-2 mt-2 justify-center items-center'>
-                <div className='text-4xl font-mono font-semibold'>PORFOLIO</div>
+                <div className='text-4xl font-mono font-semibold'>PORTFOLIO</div>
                 <div className='hidden md:flex'>
                     <div className='p-3 text-slate-400 hover:text-white cursor-pointer'>About</div>
                     <div className='p-3 text-slate-400 hover:text-white cursor-pointer'>Projects</div>
